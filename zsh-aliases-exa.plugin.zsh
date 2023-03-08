3c2738b4d0014fe498fe0b6b1f8848f1db187227
Richard Johnson <github@rjohnson.id.au>
2023-03-08 11:39:32 +1100

    add icons

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -9,13 +9,13 @@
 #
 
 # general use
-alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias ls='exa --icons'                                                         # ls
+alias l='exa -lbF --git --icons'                                               # list, size, type, git
+alias ll='exa -lbGF --git --icons'                                             # long list
+alias llm='exa -lbGF --git --sort=modified --icons'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons' # all + extended list
 
 # speciality views
 alias lS='exa -1'			                                                  # one column, just names
-alias lt='exa --tree --level=2'                                         # tree
+alias lt='exa --tree --level=2 --icons'                                         # tree
