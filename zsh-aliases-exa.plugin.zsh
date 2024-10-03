71d1461474058bdd31e8cd10ed82b92615ffd67b
Richard Johnson <github@rjohnson.id.au>
2024-10-04 08:57:33 +1000

    fix tab completion

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -9,13 +9,13 @@
 #
 
 # general use
-alias ls='eza --icons'                                                         # ls
-alias l='eza -lbF --git --icons'                                               # list, size, type, git
-alias ll='eza -lbGF --git --icons'                                             # long list
-alias llm='eza -lbGF --git --sort=modified --icons'                            # long list, modified date sort
-alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons'  # all list
-alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons' # all + extended list
+alias ls='eza --icons $@'                                                         # ls
+alias l='eza -lbF --git --icons $@'                                               # list, size, type, git
+alias ll='eza -lbGF --git --icons $@'                                             # long list
+alias llm='eza -lbGF --git --sort=modified --icons $@'                            # long list, modified date sort
+alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons $@'  # all list
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons $@' # all + extended list
 
 # speciality views
-alias lS='eza -1'			                                                  # one column, just names
-alias lt='zxa --tree --level=2 --icons'                                         # tree
+alias lS='eza -1 $@'			                                                  # one column, just names
+alias lt='zxa --tree --level=2 --icons $@'                                         # tree
